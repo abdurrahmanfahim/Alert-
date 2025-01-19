1d22c011e0de95e439ac0e2a1e6a7b7dc1d56c55
Abdur Rahman Fahim <127137394+abdurrahmanfahim@users.noreply.github.com>
2025-01-19 22:33:14 +0600

    comment added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Create a simple webpage with a button. When the button is clicked, display an alert with the text "Hello World!".  
 
 
-// showAlert function created for handling alert
+// showAlert function decleared for handling alert managed by onclick in document
 function showAlert() {
 
     // the alert
